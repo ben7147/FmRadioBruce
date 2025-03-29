9ebcbfc94330a78ba243f8f01e2510d578045df4
ben7147 <95097279+ben7147@users.noreply.github.com>
2025-03-29 13:57:03 +0100

    Create fm.h

---
+++ fm.h
@@ -0,0 +1,27 @@
+#ifndef __FM_H__
+#define __FM_H__
+
+#include <Wire.h>
+#include <globals.h>
+#include "core/display.h"
+#include "core/mykeyboard.h"
+
+// Global FM state variables
+extern uint16_t fm_station;
+extern bool is_running;
+
+// Missing function declarations added here:
+void fm_banner();
+void fm_options(uint16_t f_min, uint16_t f_max, bool reserved);
+
+void fm_live_run(bool reserved);
+void fm_ta_run();
+bool fm_begin();
+bool fm_tune(bool silent = false);
+void fm_stop();
+void fm_spectrum();
+uint16_t fm_scan();
+void set_frq(uint16_t frq);
+void set_auto_scan(bool new_value);
+
+#endif
